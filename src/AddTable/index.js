215919fc8252acd8ef44f7f8f4249eab7557831f
keremayseli <keremayseli@outlook.com>
2021-12-20 12:25:11 +0300

    UI update

--- src/AddTable/index.js
+++ src/AddTable/index.js
@@ -3,23 +3,32 @@ import { useEffect, useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Alert, InputGroup, FormControl } from 'react-bootstrap';
 
-function AddTable({MatrisX,MatrisY}) {
+function AddTable() {
     const [status, setStatus] = useState(null);
     const addNewTable = () => fetch("https://spiralinteger.azurewebsites.net/spiral/add?MatrisX="+MatrisX+"&Matrisy="+MatrisY).then((response) => setStatus(response.status));
+     const [MatrisX, setMatrisX] = useState(0);
+        const [MatrisY, setMatrisY] = useState(0);
+       const handleChangeX = (e) => {
+            setMatrisX(e.target.value);
+            console.log(MatrisX);
+        }
+        const handleChangeY = (e) => {
+            setMatrisY(e.target.value);
+            console.log(MatrisY);
+        }
 
     return (
         <>
-            {console.log(status)}
-            {status === 200 ? <Alert variant="success">Success</Alert> : null}
+            {status === 200 ? <Alert variant="success">Yeni Tablo eklendi</Alert> : null}
             <h1>Tablo</h1>
 
 
             <form>
-                <InputGroup type="number" placeholder="username" >
-                    <FormControl aria-label="Default" aria-describedby="inputGroup-sizing-default" placeholder="User Name" />
+                <InputGroup onChange={handleChangeX} name="X" type="number" placeholder="X" >
+                    <FormControl onChange={handleChangeX} name="X" aria-label="Default" aria-describedby="inputGroup-sizing-default" placeholder="X" />
                 </InputGroup>
-                <InputGroup type="number" placeholder="username">
-                    <FormControl aria-label="Default" type="number" placeholder="password" />
+                <InputGroup onChange={handleChangeY} name="Y"  type="number" placeholder="Y">
+                    <FormControl onChange={handleChangeY} name="Y" aria-label="Default" type="number" placeholder="Y" />
                 </InputGroup>
                 <Button onClick={addNewTable}>Add New Table</Button>
             </form>
